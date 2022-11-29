986d23dfc47c08339af4735e05bd9cd3bf0e10e6
Kr1s4an <krisivratza@gmail.com>
2022-11-29 11:20:57 +0200

    Spring Security

---
+++ src/main/java/com/example/Blogapplication/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.Blogapplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebSecurityConfig {
+    private static final String[] WHITELIST = {
+            "/register",
+            "/",
+            "login"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers(WHITELIST).permitAll()
+                .antMatchers(HttpMethod.GET, "/posts/*").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+
+}
