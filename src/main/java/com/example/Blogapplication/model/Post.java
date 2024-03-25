4d977a62a122c2871b522bdfa397c221d6a2c491
kr1s4an <krisivratza@gmail.com>
2024-03-25 11:05:01 +0200

    Adding phone number to the posts.

--- src/main/java/com/example/Blogapplication/model/Post.java
+++ src/main/java/com/example/Blogapplication/model/Post.java
@@ -19,6 +19,7 @@ public class Post {
     private String title;
     @Column(columnDefinition = "TEXT")
     private String body;
+    private int phoneNumber;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
